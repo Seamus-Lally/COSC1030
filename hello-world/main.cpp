6e8602112f600f184972e7c035922a97c435fe56
Seamus Lally <seamuslally@westernwyoming.edu>
2025-01-27 04:41:19 +0000

    Ready for grading

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
+// Seamus
+// 1/26/2025
 // Hello World Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
