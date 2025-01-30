23108a7f224f184f3666166044727612e122f606
Seamus Lally <seamuslally@westernwyoming.edu>
2025-01-30 18:32:25 +0000

    Ready for grading

--- ocean-levels/main.cpp
+++ ocean-levels/main.cpp
@@ -1,8 +1,20 @@
 //
-// Name
-// Date
+// Seamus Lally
+// 1/30/2025
 // Ocean Levels Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
 
+int main(){
+    //declaring variables
+    double seaLevel;
+    double raiseLevel = 1.5;
+     
+     //printing out the levels after 5, 7, and 10 years
+    cout << "After 5 years the sea level will rise " << seaLevel + (raiseLevel * 5) << " millmeters." << endl;
+    cout << "After 7 years the sea level will rise " << seaLevel + (raiseLevel * 7) << " millmeters." << endl;
+    cout << "After 10 years the sea level will rise " << seaLevel + (raiseLevel * 10) << " millmeters." << endl;
+}
