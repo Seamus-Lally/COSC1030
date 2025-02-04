754c49d18f00fea6d30b9adb930f5b39878724fc
Seamus Lally <seamuslally@westernwyoming.edu>
2025-02-04 18:55:33 +0000

    Ready to Grade

--- stadium-seating/main.cpp
+++ stadium-seating/main.cpp
@@ -1,7 +1,32 @@
 //
-// Name
-// Date
+// Seamus Lally
+// 2/4/2025
 // Stadium Seating Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+
+int main(){
+    double aSeatNum, bSeatNum, cSeatNum;
+    double aSeat, bSeat, cSeat, seatTotal ;
+    double aSeatCost = 15;
+    double bSeatCost = 12;
+    double cSeatCost = 9;
+
+    cout << "How many people bought seats for Class A: ";
+    cin >> aSeatNum;
+    cout << "How many people bought seats for Class B: ";
+    cin >> bSeatNum;
+    cout << "How many people bought seats for Class C: ";
+    cin >> cSeatNum;
+
+    aSeat = aSeatCost * aSeatNum;
+    bSeat = bSeatCost * bSeatNum;
+    cSeat = cSeatCost * cSeatNum;
+    seatTotal = aSeat + bSeat + cSeat;
+
+    cout << "the total income from seat sales is $" << fixed << setprecision(2) << seatTotal <<endl;
+}
