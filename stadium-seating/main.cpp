5582a85c898bb388b42d5e1d5227d047347081d4
Seamus Lally <seamuslally@westernwyoming.edu>
2025-02-06 18:06:37 +0000

    Ready for Grading

--- stadium-seating/main.cpp
+++ stadium-seating/main.cpp
@@ -10,12 +10,14 @@ using namespace std;
 
 
 int main(){
+    //declaring variables
     double aSeatNum, bSeatNum, cSeatNum;
     double aSeat, bSeat, cSeat, seatTotal ;
     double aSeatCost = 15;
     double bSeatCost = 12;
     double cSeatCost = 9;
 
+    //asking for the number of tickets sold
     cout << "How many people bought seats for Class A: ";
     cin >> aSeatNum;
     cout << "How many people bought seats for Class B: ";
@@ -23,10 +25,12 @@ int main(){
     cout << "How many people bought seats for Class C: ";
     cin >> cSeatNum;
 
+    //calculating the total sold
     aSeat = aSeatCost * aSeatNum;
     bSeat = bSeatCost * bSeatNum;
     cSeat = cSeatCost * cSeatNum;
     seatTotal = aSeat + bSeat + cSeat;
 
+    //displaing the total
     cout << "the total income from seat sales is $" << fixed << setprecision(2) << seatTotal <<endl;
 }
