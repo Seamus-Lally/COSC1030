bc5249141eb9c33a93127ac71cd55cae0fc04fc6
Seamus Lally <seamuslally@westernwyoming.edu>
2025-02-18 18:18:12 +0000

    Ready to Grade

--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,28 @@
 //
-// Name
-// Date
+// Seamus Lally
+// 2/18/2025
 // Calories Burned Programming Project
 // COSC 1030
 //
+
+#include <iostream>
+using namespace std;
+
+//declaring constents for loop
+const int MAX_MINUTES = 30;
+
+int main(){
+    //declaring variables
+    double cal = 3.6;
+    int burn = 0;
+    
+
+    //runs loop 6 times to calulate calories burned
+    for (int i = 5; i <= MAX_MINUTES; i += 5){
+        //calulates and displays the number of calories burned
+        burn = i * cal;
+        cout << "After " << i << " minutes you will burn " << burn << " calories." << endl;
+    }
+    
+}
 
