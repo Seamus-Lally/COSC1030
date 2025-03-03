6d94f81f208ce08be45d2b10ec6630bd6b0a7375
Seamus Lally <seamuslally@westernwyoming.edu>
2025-03-03 04:37:19 +0000

    Ready to Grade

--- change-for-a-dollar-game/main.cpp
+++ change-for-a-dollar-game/main.cpp
@@ -9,13 +9,19 @@ using namespace std;
 #include <iomanip>
 
 int main(){
+    //declaring variables
     string coin;
-    int pennies = 0;
-    int nickels = 0;
-    int dimes = 0;
-    int quarters = 0;
+    double pennies = 0;
+    double nickels = 0;
+    double dimes = 0;
+    double quarters = 0;
     int pennies_Add, nickels_Add, dimes_Add, quarters_Add;
+    double total = 0;
 
+    //stating the rules
+    cout << "Your goal is to get exactly one dollar. \nEnter penny, nickel, dime, or quarter, than the amount of them you want to add.\n";
+
+    //collecting users coins
     while (coin != "done" || coin != "Done"){
         cout << "Enter the type of coin: (Type done to stop) ";
         cin >> coin;
@@ -35,9 +41,21 @@ int main(){
             cout << "How many quarters do you want to add. ";
             cin >> quarters_Add;
             quarters += quarters_Add;
-        } else if (coin != "done" || coin != "Done"){
+        } else if (coin == "done" || coin == "Done"){
+            break;
+        }else{
             cout << "invalid input. \n";
         }
+    }
+    //calculating total
+    total = (pennies * 0.01) + (nickels * 0.05) + (dimes * 0.10) + (quarters * 0.25);
+    //printing results
+    if (total == 1){
+        cout << "You win. \n";
+    }else if (total <= 1){
+        cout << "You were under a dollor by $" << fixed << setprecision(2) << (1 - total) << endl;
+    }else{
+        cout << "You were over a dollor by $" << fixed << setprecision(2) << (total - 1) << endl;
     }
 }
 
