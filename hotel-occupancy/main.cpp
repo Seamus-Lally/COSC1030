463de6d5fd6b7d5021633a7d396e3a4242d4971d
Seamus Lally <seamuslally@westernwyoming.edu>
2025-03-03 07:03:02 +0000

    Ready to grade (added comments)

--- hotel-occupancy/main.cpp
+++ hotel-occupancy/main.cpp
@@ -10,11 +10,12 @@ using namespace std;
 #include <iomanip>
 
 int main(){
+    //declaring variables
     int floor, room, occupied;
     double total_Occupied = 0;
     double total_Room = 0;
 
-
+    //getting the number of floors
     while(true){
         cout << "Enter the number of floors: ";
         cin >> floor;
@@ -23,10 +24,11 @@ int main(){
         }
         cout << "The number of floors needs to be bigger than 1. \n";    
     }
-
+    //loop for each floor (not including 13)
     for (int i = 1; i <= floor; i++){
         if (i != 13){
             cout << "Floor: " << i <<endl;
+            //getting number of rooms on a floor
             while(true){
                 cout << "Enter the number of rooms: (can't be less than 10) ";
                 cin >> room;
@@ -36,7 +38,7 @@ int main(){
                 }
                 cout << "The number of rooms needs to be more than 10.\n";
             }
-
+            //getting a number of occupied rooms
             while(true){
                 cout << "How many of the " << room << " rooms are occupied? ";
                 cin >> occupied;
@@ -48,6 +50,7 @@ int main(){
             }
         }
     }
+    //showing how many rooms the hotel has, how many of them are occupied, how many are unoccupied, and the percentage of rooms that are occupied
     cout << "The number of rooms is: " << total_Room << endl;
     cout << "The number of occupied rooms is: " << total_Occupied << endl;
     cout << "The number of unoccupied rooms is: " << total_Room - total_Occupied << endl;
