3388d044d9b11fdc374b6a85c731724d591607ea
Seamus Lally <seamuslally@westernwyoming.edu>
2025-03-17 03:15:25 +0000

    Ready to Grade

--- markup/main.cpp
+++ markup/main.cpp
@@ -12,19 +12,22 @@ using namespace std;
 double calculateRetail(double, double);
 
 int main(){
+    //declaring variables
     double wholeCost, finCost, markup;
 
-
+    //getting wholesale cost and markup %
     cout << "Enter the wholesale cost of the item: $";
     cin >> wholeCost;
     cout << "Enter the percent markup of the item: (Not in decimal) ";
     cin >> markup;
 
+    //getting and displaing retail cost
     finCost = calculateRetail(wholeCost, markup); 
     cout << "The items retail cost is $" << fixed << setprecision(2) << finCost << endl;
 
 }
 
+//calculation retail cost
 double calculateRetail(double x, double y){
     double z;
     z = x * ((100 + y)/100);
