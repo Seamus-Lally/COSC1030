d9707055cb5a2a4daf38fe426e1c075316e1750d
Seamus Lally <seamuslally@westernwyoming.edu>
2025-03-17 03:10:50 +0000

    Ready to Grade

--- markup/main.cpp
+++ markup/main.cpp
@@ -1,7 +1,32 @@
 //
-// Name
-// Date
+// Seamus Lally
+// 3/16/2025
 // Markup Programming Project
 // COSC 1030
 //
 
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+double calculateRetail(double, double);
+
+int main(){
+    double wholeCost, finCost, markup;
+
+
+    cout << "Enter the wholesale cost of the item: $";
+    cin >> wholeCost;
+    cout << "Enter the percent markup of the item: (Not in decimal) ";
+    cin >> markup;
+
+    finCost = calculateRetail(wholeCost, markup); 
+    cout << "The items retail cost is $" << fixed << setprecision(2) << finCost << endl;
+
+}
+
+double calculateRetail(double x, double y){
+    double z;
+    z = x * ((100 + y)/100);
+    return z;
+} 
