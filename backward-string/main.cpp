35166b0bde1b51d092829c19b0df2721a0feaf10
Seamus Lally <seamuslally@westernwyoming.edu>
2025-04-21 04:18:40 +0000

    Ready to grade

--- backward-string/main.cpp
+++ backward-string/main.cpp
@@ -1,7 +1,33 @@
 //
-// Name
-// Date
+// Seamus Lally
+// 4/20/2025
 // Backward String Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+void backward(char* str);
+
+int main(){
+
+    const int size = 50;
+    char input[size];
+
+    cout << "enter a word under 50 characters: \n";
+    cin.getline(input, size);
+
+    backward(input);
+    return 0;
+}
+
+void backward(char* str){
+    int length = strlen(str);
+
+    for(int i = length; i >= 0; i--){
+        cout << str[i];
+    }
+    cout << endl;
+    
+}
